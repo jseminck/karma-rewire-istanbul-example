86f9e846af8630624daa4957287d268ae8813400
Joachim Seminck <joachim.seminck@zeroturnaround.com>
2016-08-14 17:28:32 +0300

    Split the tests into mock test and unmocked test.

--- tests/AppTest.js
+++ tests/AppTest.js
@@ -2,7 +2,7 @@ import React from "react";
 import { shallow } from 'enzyme';
 import App from './../src/App';
 
-describe("App", function() {
+describe("App without mocking", function() {
   it("contains h1 tag", function() {
     const wrapper = shallow(<App />);
     expect(wrapper.find("h1").length).toEqual(1);
@@ -12,5 +12,26 @@ describe("App", function() {
     const wrapper = shallow(<App />);
     const dependencyValue = wrapper.find(".dependency").get(0).props.children;
     expect(dependencyValue).toEqual(12);
+  });
+});
+
+describe("App with mocking", function() {
+  beforeEach(function() {
+    App.__set__({
+      dependency:{
+        value: 100
+      }
+    });
+  });
+
+  it("contains h1 tag", function() {
+    const wrapper = shallow(<App />);
+    expect(wrapper.find("h1").length).toEqual(1);
+  });
+
+  it("contains the mocked out dependency value", function() {
+    const wrapper = shallow(<App />);
+    const dependencyValue = wrapper.find(".dependency").get(0).props.children;
+    expect(dependencyValue).toEqual(100);
   });
 });
