dfa3bdbef3d37013ad171c836a53ce212190a2b1
Joachim Seminck <joachim.seminck@zeroturnaround.com>
2016-08-15 08:01:21 +0300

    Fix coverage location

--- karma.conf.js
+++ karma.conf.js
@@ -22,7 +22,7 @@ module.exports = function(config) {
 
     coverageReporter: {
       reporters: [
-        {type: "html", dir: "../coverage/"},
+        {type: "html", dir: "./coverage/"},
         {type: "text-summary"}
       ]
     },
